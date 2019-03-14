be24d37ae4160e9132f3486d685ba9db66e801b2
MerlijnKickMetman <48549277+MerlijnKickMetman@users.noreply.github.com>
2019-03-14 11:00:00 +0100

    Add files via upload

---
+++ Rekentuin app code/Rekentuin/Rekentuin/platforms/android/assets/www/scripts/index.js
@@ -0,0 +1,12 @@
+﻿function tafels() {
+    let Keuze = Nmmr1;
+    let tmpvar = "";
+    for (x = 1; x < 11; x++) {
+        tmpvar += '<p id="Tafels' + x + '"></p>';
+    }
+    document.getElementById("Demo").innerHTML = tmpvar;
+    for (x = 1; x < 11; x++) {
+        Uitkomst = Keuze * x;
+        document.getElementById("Tafels" + x).innerHTML = x + " x " + Keuze + " = " + Uitkomst;
+    }
+}
